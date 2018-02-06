578a7b3553c8375005a2673424eea49060f09a24
agedob <agedob888@gmail.com>
2018-02-05 18:59:39 -0800

    done.

--- main.py
+++ main.py
@@ -2,21 +2,21 @@
 list_one = [1,2,5,6,2]
 list_two = [1,2,5,6,2]
 
-def list_comp(arg,arg2)
+def list_comp(arg, arg2):
     count = 0
-    if len(arg) == len(arg2)
+    if len(arg) != len(arg2):
         print "The lists are not the same."
-        break
     else:
-        for x in arg():
-            if arg[i] == arg2[i]:
-                count += 1
+        for x in range(0, len(arg), 1):
+            if arg[x] == arg2[x]:
+                count = count + 1
             else:
                 break
         if count == len(arg):
             print "The lists are the same."
+            print count
         else:
             print "The lists are not the same."
-            break
-            
+            print count
+
 list_comp(list_one,list_two)
