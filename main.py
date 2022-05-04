e5cdbe929e67bac2d67aa9ef0d355ac11a158231
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-04 10:08:18 -0700

    tweaked a little

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+list = [
+        [[1,2,5,6,2],
+        [1,2,5,6,2]],
 
-list_one = [1,2,5,6,2]
-list_two = [1,2,5,6,2]
+        [[1,2,5,6,5],
+        [1,2,5,6,5,3]],
+
+        [[1,2,5,6,5,16],
+        [1,2,5,6,5]],
+
+        [['celery','carrots','bread','milk'],
+        ['celery','carrots','bread','cream']]
+]
 
 def list_comp(arg, arg2):
     count = 0
     if len(arg) != len(arg2):
-        print "The lists are not the same."
+        print("The lists are not the same.")
     else:
         for x in range(0, len(arg), 1):
             if arg[x] == arg2[x]:
@@ -13,10 +23,12 @@ def list_comp(arg, arg2):
             else:
                 break
         if count == len(arg):
-            print "The lists are the same."
-            print count
+            print("The lists are the same.")
+            print(count)
         else:
-            print "The lists are not the same."
-            print count
+            print("The lists are not the same.")
+            print(count)
 
-list_comp(list_one,list_two)
+list_comp(list[0][0], list[0][1])
+
+# print(list[0])
